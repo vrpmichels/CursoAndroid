7aaf163082bdceec2f2b09eb17723bee4eb43fe2
vrpmichels <94145954+vrpmichels@users.noreply.github.com>
2023-05-15 15:06:31 -0300

    04. Trabalhando com arquivo fonte do layout
    Adicionar ícones no projeto;
    Implementar layout via XML;
    Implementar layouts com o LinearLayout;
    Implementar layout de formulário;
    Refatorar Activities em relação ao pacote e nomes;
    Implementar App Bar e uso do AppCompatActivity.

--- Agenda/app/src/main/java/curso/java/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/curso/java/ui/activity/ListaAlunosActivity.java
@@ -1,28 +1,27 @@
-package curso.java;
+package curso.java.ui.activity;
 
-import android.app.Activity;
+
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+import curso.java.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
       //  Toast.makeText(this, "Vinicius", Toast.LENGTH_LONG).show();
-        setContentView(R.layout.activity_main);  // R faz mapeamento de todos o layout do projeto
+        setContentView(R.layout.activity_lista_alunos);  // R faz mapeamento de todos o layout do projeto
+        setTitle("Lista de Aluno");
         List<String> alunos = new ArrayList<>(Arrays.asList("Vinicius", "Renan", "Michels", "Alice", "Batata"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_de_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
